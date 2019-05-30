8c274ef08595ff386fdd8ec78495b5099db0129e
zasdx1 <zasdx1@naver.com>
2019-05-30 17:54:23 +0900

    문제1

--- 백준/백준함수.py
+++ 백준/백준함수.py
@@ -21,14 +21,36 @@ else :
 '''
 #문제 1 셀프넘버 / 각자리수 0~9까지 모든경우 배열 / 최종 for문에서 나온값이 없을경우 print
 
+D1 = []
+D2 = []
+D3 = []
+D4 = []
 
-N = input()
+
+for i in range(1,10000):
+    K = str(i)
+
+    if i<10:
+        F = 2*i
+        D1.append(F)
+        
+    elif i<100:
+        F = i + int(K[0])+int(K[1])
+        D2.append(F)
+    elif i<1000:
+        F = i + int(K[0])+int(K[1])+int(K[2])
+        D3.append(F)
+    else :
+        F = i + int(K[0])+int(K[1])+int(K[2])+int(K[3])
+        D4.append(F)
+
+pre_D_sum = D1+D2+D3+D4
+D_sum = set(pre_D_sum)
+D_sum = sorted(D_sum)
+print(D_sum)
 
 
-if true:
-    for i in range(1,int(N)+1)
-        K = str(i)
-    
+
 
 
 
